a4264dee9af209976f2c3809e0296506958cd2e9
akagaeng <akagaeng@gmail.com>
2019-07-24 22:54:24 +0900

    #6 Error 화면 렌더 에러 나지 않도록 수정

--- app.js
+++ app.js
@@ -66,7 +66,11 @@ app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', {
+        title: "Error",
+        user: req.user,
+        loginError: req.flash('loginError'),
+    });
 });
 
 app.listen(app.get('port'), () => {
