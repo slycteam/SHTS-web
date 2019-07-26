d581ad51f6b1450b48d9c631eed563d11a0cdb09
Martin <hunyou82@gmail.com>
2019-07-27 00:38:36 +0900

    middleware

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -3,7 +3,7 @@ exports.isLoggedIn = (req, res, next) => {
         next();
     } else {
         // res.status(403).send('로그인 필요');
-        res.redirect('/');
+        res.redirect('/login');
     }
 };
 
